3e66fcc7027758e2d21018eefd11d2d1c4aa26d2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-02-09 15:01:18 -0600

    Initial Commit

---
+++ ChartJump/ChartJumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartJumpApp.swift
+//  ChartJump
+//
+//  Created by Mark Volkmann on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartJumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
